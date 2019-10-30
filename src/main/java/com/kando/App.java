be48cba5cf84aa3993fa76ea844d716df5894cfc
root <1058808876@qq.com>
2019-10-30 14:38:04 +0800

    10/30 14:37 孙雨佳

--- src/main/java/com/kando/App.java
+++ src/main/java/com/kando/App.java
@@ -5,6 +5,7 @@ import com.kando.entity.User;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -15,6 +16,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableFeignClients
 @EnableEurekaClient
 @EnableRedisHttpSession
+@EnableCaching
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
